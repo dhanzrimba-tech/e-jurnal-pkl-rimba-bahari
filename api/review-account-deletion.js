760e702e47a8b8d4042f4877cc7dba7cb3e781e4
dhanzrimba-tech <dhanzrimba@gmail.com>
2026-09-02 11:17:04 +0700

    Delete api/review-account-deletion.js

--- api/review-account-deletion.js
+++
@@ -1,8 +0,0 @@
-import { allowPostOnly, sendJson } from './_supabase.js';
-
-export default async function handler(req, res) {
-  if (!allowPostOnly(req, res)) return;
-  return sendJson(res, 410, {
-    error: 'Alur persetujuan penghapusan akun sudah dinonaktifkan. Gunakan penghapusan langsung oleh administrator.',
-  });
-}
